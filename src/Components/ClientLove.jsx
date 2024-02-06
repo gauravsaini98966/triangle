07c85a147ab604136461f2d16c0c59db88e34ff6
sainideepanshu1 <deepanshusaini.saini5@gmail.com>
2024-02-06 10:01:02 +0530

    slider

--- src/Components/ClientLove.jsx
+++ src/Components/ClientLove.jsx
@@ -3,26 +3,29 @@ import { Client_top } from "../Data/Client_Data";
 import { Client_bottom } from "../Data/Client_Data";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Autoplay } from "swiper/core";
+
 function ClientLove() {
   return (
-    <div className="Client">
-      <div className="Client_top">
-        {Client_top.map((product, index) => {
-          return (
-            <div key={index} className="Client_top1">
-              <h3>{product.name}</h3>
-              <h2>{product.name2}</h2>
-            </div>
-          );
-        })}
-      </div>
-
-      <div>
+    <>
+      <div className="Client">
+        <div className="Client_top">
+          {Client_top.map((product, index) => {
+            return (
+              <div key={index} className="Client_top1">
+                <h3>{product.name}</h3>
+                <h2>{product.name2}</h2>
+              </div>
+            );
+          })}
+        </div>
         <div className="Client_bottom">
           {Client_bottom.map((product, index) => {
             return (
               <div className="" key={index}>
-                {/* <Slider {...settings}> */}
                 <div className="Client_bottom1">
                   <img src={product.img} alt="not found" />
                   <p>{product.p}</p>
@@ -38,14 +41,47 @@ function ClientLove() {
                   <h2>{product.name}</h2>
                   <h3>{product.position}</h3>
                 </div>
-
-                {/* </Slider> */}
               </div>
             );
           })}
         </div>
       </div>
-    </div>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        centeredSlides={true}
+        rewind={true}
+        loop={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        modules={[Autoplay]}
+        className="mySwiper"
+      >
+        {Client_bottom.map((product, index) => (
+          <SwiperSlide key={index}>
+            <div className="client-slider">
+              <div className="Client_bottom1">
+                <img src={product.img} alt="not found" />
+                <p>{product.p}</p>
+
+                <div className="Client_bottom_star">
+                  {product.img2.map((star, starindex) => (
+                    <img key={starindex} src={star} alt="" />
+                  ))}
+                </div>
+              </div>
+              <div className="Owner">
+                <img src={product.img3} alt="" />
+                <h2>{product.name}</h2>
+                <h3>{product.position}</h3>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
   );
 }
 
